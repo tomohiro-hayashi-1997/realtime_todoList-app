1e1ec554b2cf9d947a31f718ff814cb7a3a82711
tomohiro-hayashi-1997 <tomohiro.hayashi.1997@gmail.com>
2021-08-15 22:00:18 +0900

    全てやり直し

--- js/main.js
+++ js/main.js
@@ -5,10 +5,14 @@ const app = new Vue({
       inputText: '',
       lists: [
         {
-          name: 'XX作業',
+          name: 'ネギを買う',
           isDone: false
         }
-      ]
+      ],
+      min: 59,
+      sec: 59,
+      timerOn: false,
+      timerObj: null,
     };
   },
   methods: {
@@ -24,6 +28,47 @@ const app = new Vue({
     onDelete (index) {
       this.lists.splice(index, 1);
     },
+    
+    count: function() {
+      if (this.sec <= 0 && this.min >= 1) {
+        this.min --;
+        this.sec = 59;
+      } else if(this.sec <= 0 && this.min <= 0) {
+        this.complete();
+      } else {
+        this.sec --;
+      }
+    },
+
+    start: function() {
+      let self = this;
+      this.timerObj = setInterval(function() {self.count()}, 1000)
+      this.timerOn = true; //timerがONであることを状態として保持
+    },
+
+    stop: function() {
+      clearInterval(this.timerObj);
+      this.timerOn = false; //timerがOFFであることを状態として保持
+    },
+
+    complete: function() {
+      clearInterval(this.timerObj)
+    }
   },
 
+  computed: {
+    formatTime: function() {
+      let timeStrings = [
+        this.min.toString(),
+        this.sec.toString()
+      ].map(function(str) {
+        if (str.length < 2) {
+          return "0" + str
+        } else {
+          return str
+        }
+      })
+      return timeStrings[0] + ":" + timeStrings[1]
+    }
+  }
 });
